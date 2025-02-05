d791473479785f8ce835cdc2f77f7da0ce922c19
Chicão Thiago <fthiagogv@gmail.com>
2025-02-04 21:07:35 -0300

    test: ✅ add h1 test

--- src/components/ui/text/H1.spec.tsx
+++ src/components/ui/text/H1.spec.tsx
@@ -1,3 +1,13 @@
-describe("H1", () => {
-  it("should render the children", () => {});
+import { render, screen } from '@testing-library/react'
+import { H1 } from './H1';
+
+describe("<H1 />", () => {
+  const childrenMock = 'text mock'
+  it("should render the children", () => {
+    render(<H1>{childrenMock}</H1>)
+    const element = screen.getByText(childrenMock)
+    expect(element).toBeDefined()
+    expect(element.classList).toContain('font-bold')
+    expect(element.classList).toContain('text-4xl')
+  });
 });
